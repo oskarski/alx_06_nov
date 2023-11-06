c0ad4669b35fc8f8878d007bdd168dd77a25d14a
Oskar ALX Kupski <oskar.kupski@gmail.com>
2023-11-06 12:24:53 +0100

    Object functions & destruct...

--- class.js
+++ class.js
@@ -9,12 +9,24 @@ const secondPerson = {
     password: '1234'
 };
 
+
+// w TS *
+// class Foo {
+//     constructor(
+//         private readonly name: string,
+//     ) {}
+// }
+
 class SignInDto {
     keepSignedIn = true;
 
     constructor(login, password) {
         this.login = login;
         this.password = password;
+    }
+
+    print() {
+        console.log(this.login + ' ' + this.password)
     }
 }
 
@@ -25,6 +37,7 @@ const secondDto = new SignInDto('maciek', '1qaz');
 console.log(dto instanceof SignInDto);
 console.log(obj instanceof SignInDto);
 console.log(secondDto);
+secondDto.print();
 
 class NotFoundError extends Error {}
 
@@ -50,3 +63,86 @@ try {
         console.error(err)
     }
 }
+
+
+console.log(
+    Object.keys({ login: 'oskar', password: '1234' })
+);
+
+console.log(
+    Object.keys(dto)
+);
+
+
+console.log(
+    Object.values({ login: 'oskar', password: '1234' })
+);
+
+console.log(
+    Object.values(dto)
+);
+
+console.log(
+    Object.entries({ login: 'oskar', password: '1234' })
+);
+
+console.log(
+    Object.entries(dto)
+);
+
+for (const entry of Object.entries(dto)) {
+    console.log({ klucz: entry[0], wartosc: entry[1] })
+}
+
+for (const entry of Object.entries(dto)) {
+    console.log({ klucz: entry[0], wartosc: entry[1] })
+}
+
+for (const entry of Object.entries(dto)) {
+    // destrukturyzacja tablicy
+    const [first, second] = entry;
+    // w react
+    // const [state, setState] = useState();
+
+    console.log({ klucz: first, wartosc: second });
+}
+
+for (const [first, second] of Object.entries(dto)) {
+    console.log({ klucz: first, wartosc: second });
+}
+
+// destrukturyzacja tablicy
+const [first, second] = ['pierwszy element', 'drugi element'];
+// w react
+// const [state, setState] = useState();
+
+// destrukturyzacja obiektu
+const { login } = dto;
+console.log(login);
+// dto.login
+
+// Praktyka w react *
+// const LoginForm = (props) => {
+//     return <div id={props.id }></div>;
+// }
+// {/* <LoginForm id='login-form' /> */}
+// const LoginForm = (props) => {
+//     const { id } = props;
+
+//     return <div id={id}></div>;
+// }
+
+// {/* <LoginForm id='login-form' /> */}
+// const LoginForm = ({ id }) => {
+//     return <div id={id}></div>;
+// }
+
+
+const arr = [];
+
+arr['arr_first'] = '11111';
+arr.arr_second =  '22222'
+
+const { arr_first, arr_second } = arr;
+console.log(arr_first, arr_second);
+console.log(arr.arr_first);
