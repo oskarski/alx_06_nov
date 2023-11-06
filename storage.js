f0fe67b223f493defdf99113d94f9fafe9027f87
Oskar ALX Kupski <oskar.kupski@gmail.com>
2023-11-06 14:04:51 +0100

    storage

---
+++ storage.js
@@ -0,0 +1,17 @@
+// Local Storage
+
+localStorage.setItem('language', 'pl');
+
+console.log(
+    localStorage.getItem('language')
+);
+
+// localStorage.clear() -> usun wszystko
+// localStorage.removeItem('language');
+
+// Session Storage
+// sessionStorage.setItem('lng', 'pl');
+
+console.log(
+    sessionStorage.getItem('lng')
+)
